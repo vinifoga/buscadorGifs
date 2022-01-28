ea7072d3f21233865fdff675429abb37f85f7010
Vinicius Fogaca <viniciusfernandes38@gmail.com>
2022-01-28 05:31:32 -0300

    Searching input

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,18 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: HomePage(),
+    home: const HomePage(),
+    debugShowCheckedModeBanner: true,
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+          labelStyle: TextStyle(color: Colors.white),
+        )),
   ));
 }
